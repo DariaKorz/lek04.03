2864774fec794f7757373ca9af86bba9b91395a1
DariaKorz <dariaoliwia1295@gmail.com>
2021-03-04 13:47:22 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+a = 5
+b = 3.15
+
+c, d, e = 'a', 5, 3.5
+print(c)
+print(d)
+print(e)
+
+v = 3 + 3j
+print(v)
+
+print(type(b))
+
+liczba_a = 9
+
+napis = 'abcdef'
+napis2 = 'lol'
+print(napis+napis2)
+
+dzielenie = a / c
+
+dzielenie_calkowite = a // c
+
+reszta_z_dzielena = a % c
+
+potegowanie = a**2
+print(potegowanie)
+
+print(pow(4, 3))
+
+a+=2
+print(a)
+
+t = 7.4567
+print(round(t))
+
+print(sqrt(liczba_a))
---
+++ n.py
@@ -0,0 +1,12 @@
+print("wynik działania jest równy a=%(za)d" % {'zn' : 12})
+
+specjalizacja = "informatyka ogólna"
+rok = 1
+
+print("nazwa specjalizacji: Ns, rok: %d" %(specjalizacja, rok))
+
+a = 5
+b = 3
+c = 5 - 3
+
+print("wynik działania %(z1)d - %(z2)d - %(z3)d" %{'z1': a, 'z2': b, 'z3': c})
